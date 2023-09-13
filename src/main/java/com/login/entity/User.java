dbfe117e0edf3c686d3c6e223932896a0e6caa9a
khandekaranjali403 <ratankhandekar2@gmail.com>
2023-09-13 20:07:27 +0530

    user-login-backend

--- src/main/java/com/login/entity/User.java
+++ src/main/java/com/login/entity/User.java
@@ -6,16 +6,14 @@ import javax.persistence.Table;
 
 import lombok.Data;
 
-@Entity(name="userdetails")
+@Entity
 @Data
 @Table(name="User")
 public class User {
 	
 	@Id
-	private int id;
 	private String username;
 	private String password;
-	//private String email;
 	
 
 }
